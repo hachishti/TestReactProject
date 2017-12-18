e240633efbbfd3da0871c05ab72bc7e07b890164
hachishti <34640012+hachishti@users.noreply.github.com>
2017-12-18 10:02:31 +0400

    Initial Commit v1.0
    This is the test project

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+let varDefaultTextColor = '#fff';
+let varDefaultStyle = {
+  color: varDefaultTextColor
+}
+class ChildClass extends Component{
+render(){
+return(
+    <div style={{width: '20%', display: 'inline-block', ...varDefaultStyle}}>
+    <h2>Number Text</h2>
+    </div>
+    );
+  }
+}
+
+class Filters extends Component{
+  render(){
+    return(
+    <div style={varDefaultStyle}>
+      <img/>
+      <input type="text"/>
+      
+    </div>        
+    );
+  }
+}
+
+class PlayLists extends Component{
+  render(){
+    return(
+      <div style={{...varDefaultStyle, width: "20%", display: 'inline-block'}}>
+        <img/>
+        <h3>Playlist Name</h3>
+        <ul>
+          <li>Song 1</li>  <li>Song 2</li>  <li>Song 3</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1 style={varDefaultStyle}>Title</h1>
+        <ChildClass/>
+          <ChildClass/>
+          <Filters/>
+          <PlayLists/>
+          <PlayLists/>
+          <PlayLists/>
+          <PlayLists/>
+      </div>
+    );
+  }
+}
+
+export default App;
